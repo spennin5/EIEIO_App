1023d4c6ac66815523b9ba707b641354c6d4e915
Sam Pennington <sam.pennington5@gmail.com>
2021-08-23 13:41:14 -0400

    Finished Save User function

--- Components/DataConnector.js
+++ Components/DataConnector.js
@@ -13,20 +13,40 @@ export class User{
   printUser(){
     console.log(this);
   }
-  //Save current user to a local JSON file
-  async saveUser(){
-    let userObj = JSON.stringify(this);
-    await fs.writeAsStringAsync(fs.documentDirectory+'user.txt',userObj);
-
-  }
   /*TODO fill in function to load users. Due to localized storage rules,
   you can't append to a file without loading its contents and re-saving the whole thing first.
   So, to save a new user, we must load the old users with this function, turn them to an object,
   add the new user, JSON. stringify, and then resave
   */
   async loadUsers(){
-
+    const retrievedObj = await fs.readAsStringAsync(fs.documentDirectory+"user.txt");
+    return retrievedObj;
   }
+  //Save current user to a local JSON file
+  async saveUser(){
+    //await fs.deleteAsync(fs.documentDirectory+"user.txt");
+
+    const fileInfo = await fs.getInfoAsync(fs.documentDirectory+"user.txt");
+    let newUserObj = {}
+    newUserObj[String(this.username)] = JSON.stringify(this);
+    if(fileInfo.exists){
+      let oldUserObj = await this.loadUsers();
+        //.then((oldUserObj)=>console.log(oldUserObj));
+
+      oldUserObj = JSON.parse(oldUserObj);
+
+      oldUserObj[String(this.username)] = JSON.stringify(newUserObj[String(this.username)]);
+      console.log(oldUserObj);
+      toSaveUserObj = JSON.stringify(oldUserObj);
+      await fs.writeAsStringAsync(fs.documentDirectory+'user.txt',toSaveUserObj);
+    }
+    else{
+      console.log("Writing new user file");
+      newUserObj = JSON.stringify(newUserObj);
+      await fs.writeAsStringAsync(fs.documentDirectory+'user.txt',newUserObj);
+    }
+  }
+
 }
 
 export function saveUser(user){
