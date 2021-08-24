2349569b7c39e2cfafacc9816627f1d6218bb4e0
Sam Pennington <sam.pennington5@gmail.com>
2021-08-24 13:10:01 -0400

    Dynamically loading pre-made items

--- ForDemo/demoItemsFunctions.js
+++ ForDemo/demoItemsFunctions.js
@@ -5,13 +5,39 @@ var itemJson = require('./demoItems.json')
 
 export default function DemoItems(props){
   let itemsInCode = itemJson[props.zipCode];
-  console.log(itemsInCode)
+  let keyArray = Object.keys(itemJson[props.zipCode]);
+  console.log(Object.entries(itemsInCode))
+  let itemString = "";
+  for(const [key,value] of Object.entries(itemsInCode)){
+    console.log("key:"+ key);
+    React.createElement(ItemCard,{price:value.price})
+    console.log(key);
+    console.log(value.price);
+  }
+
+
+
   return(
-    <View></View>
+    <View>
+    {
+      keyArray.map((key)=>{
+        console.log("Item:"+ itemsInCode[key].item);
+        return(
+          <ItemCard item={itemsInCode[key].item} seller={key} price={itemsInCode[key].price}/>
+        )
+      })
+    }
+
+
+
+
+    </View>
   );
-  /*return(
-    {itemsInCode.map(item=>(
-      <ItemCard price
-    ))}
-  );*/
+}
+function IndividualItem(props){
+  return(
+    <View>
+    <ItemCard price={props.price}/>
+    </View>
+  );
 }
