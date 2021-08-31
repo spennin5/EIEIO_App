79be93c16f2427aada2954139307f3b3b1bf89eb
Sam Pennington <sam.pennington5@gmail.com>
2021-08-31 15:34:03 -0400

    Created multiple pins with callouts in Athens

--- Pages/MapPage.js
+++ Pages/MapPage.js
@@ -1,27 +1,34 @@
 import * as React from 'react';
 import { View, Text, TextInput, Button,Modal, TouchableOpacity,StyleSheet} from 'react-native';
-import MapView, {Marker} from 'react-native-maps';
+import MapView, {Marker,Callout} from 'react-native-maps';
 
 var itemJson = require('../ForDemo/demoItems.json')
 
 export default function MapPage(props){
   let itemsInCode = itemJson[props.zipCode];
-  console.log(props)
-  console.log(itemsInCode)
+  let markers = [];
   try{
     let keyArray = Object.keys(itemJson[props.zipCode]);
+    keyArray.map((key)=>(
+      markers.push({title:itemsInCode[key].item,price:itemsInCode[key].price, coordinates:{
+        latitude:itemsInCode[key].lat,
+        longitude:itemsInCode[key].long
+      }})
+    ))
+    console.log("Markers: "+markers)
     return(
       <View style={styles.container}>
-        <MapView style={{width: "100%", height: "50%"}}>
+        <MapView style={styles.map}
+        initialRegion={mapInitialRegion}>
         {
-          keyArray.map((key)=>{
-            console.log("Item: "+itemsInCode[key].lat)
-            console.log("Item: "+itemsInCode[key].long)
-            return(
-              <Marker key={itemsInCode[key].item} coordinate={{latitude:-84.2,longitude:34.2}}/>
-            )
-          })
-        }
+          markers.map((marker,index)=>(
+              <Marker key={marker.title} coordinate={marker.coordinates} title={marker.title}>
+                <Callout>
+                  <Text>{marker.title}</Text>
+                  <Text>{marker.price}</Text>
+                </Callout>
+              </Marker>
+          ))}
         </MapView>
       </View>
     );
@@ -40,5 +47,15 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  map:{
+    width: "100%",
+    height: "90%"
   }
 })
+const mapInitialRegion = {
+  latitude: 33.92555,
+  longitude: -83.37327,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421
+}
