6b0efe52992f49c04dedc7456754c359fe52ff76
Sam Pennington <sam.pennington5@gmail.com>
2021-10-22 12:36:48 -0400

    Attached transaction and confirmation pages

--- Pages/Transaction.js
+++ Pages/Transaction.js
@@ -1,26 +1,58 @@
 import * as React from 'react';
 import { StyleSheet, View, Text, Image,TouchableOpacity,Modal,Button,TextInput, KeyboardAvoidingView, ScrollView } from 'react-native';
 
-export default function TransactionPage(props){
+export default function TransactionPage({navigation,route,props}){
+  const [ccNum, setCCNum] = React.useState();
+  const [ccv, setCcv] = React.useState(null);
+  const [billingAddress, setBillingAddress] = React.useState("");
+  const [city, setCity] = React.useState("");
+  const [state, setState] = React.useState("");
+  const [email, setEmail] = React.useState("");
 
+  function validatePayment(){
+    console.log(String(ccNum).length)
+    if(!(String(ccNum).length==16)){
+      alert("Please Fill in Credit Card")
+    }
+    else if(!(String(ccv).length==3)){
+      alert("Please Fill in CVV")
+    }
+    else if(billingAddress==""){
+      alert("Please Fill in Billing Address")
+    }
+    else if(city==""){
+      alert("Please Fill in City")
+    }
+    else if(state==""){
+      alert("Please Fill in State")
+    }
+    else if(email==""){
+      alert("Please Fill in Email")
+    }
+    else{
+
+      navigation.navigate("OrderConfirm")
+    }
+  }
   return(
     <KeyboardAvoidingView enabled={true}>
       <ScrollView style={styles.modal}>
-        <Text style={styles.verifyItemsText}>You're buying {props.item} from {props.seller} for {props.price}!</Text>
+        <Text style={styles.verifyItemsText}>Your total is ${route.params.total}!</Text>
         <Text style={styles.confirmationText}>Confirmation and pick up details will be sent upon transaction completion.</Text>
         <View style={styles.transactionForm}>
           <Text style={styles.label}>Card Number:</Text>
-          <TextInput style={styles.input} placeholder='Credit/Debit Card Number' placeholderTextColor='#727274'></TextInput>
+          <TextInput onChangeText={text=>setCCNum(text)} style={styles.input} placeholder='Credit/Debit Card Number' placeholderTextColor='#727274'></TextInput>
           <Text style={styles.label}>CVV:</Text>
-          <TextInput style={styles.input} placeholder='CVV' placeholderTextColor='#727274'></TextInput>
+          <TextInput onChangeText={text=>setCcv(text)} style={styles.input} placeholder='CVV' placeholderTextColor='#727274'></TextInput>
           <Text style={styles.label}>Billing Address:</Text>
-          <TextInput style={styles.input} placeholder='Billing Address' placeholderTextColor='#727274'></TextInput>
+          <TextInput onChangeText={text=>setBillingAddress(text)} style={styles.input} placeholder='Billing Address' placeholderTextColor='#727274'></TextInput>
           <Text style={styles.label}>City:</Text>
-          <TextInput style={styles.input} placeholder='City' placeholderTextColor='#727274'></TextInput>
+          <TextInput onChangeText={text=>setCity(text)} style={styles.input} placeholder='City' placeholderTextColor='#727274'></TextInput>
           <Text style={styles.label}>State:</Text>
-          <TextInput style={styles.input} placeholder='GA' placeholderTextColor='#727274'></TextInput>
+          <TextInput onChangeText={text=>setState(text)} style={styles.input} placeholder='GA' placeholderTextColor='#727274'></TextInput>
           <Text style={styles.label}>Confirmation Email Address:</Text>
-          <TextInput style={styles.input} placeholder='Email Address' placeholderTextColor='#727274'></TextInput>
+          <TextInput onChangeText={text=>setEmail(text)} style={styles.input} placeholder='Email Address' placeholderTextColor='#727274'></TextInput>
+          <Button title="Submit" onPress={()=>validatePayment()}>Submit!</Button>
         </View>
       </ScrollView>
     </KeyboardAvoidingView>
